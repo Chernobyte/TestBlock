44fb56aab18b189670224740efd5d8617c04c308
Chernobyte <miles@failsafecontrols.com>
2019-08-23 16:21:15 -0500

    made firstblock directional, added a unique front texture, and ability to plafce in a similar style to pistons

--- src/main/java/com/chernobyte/testblock/blocks/FirstBlock.java
+++ src/main/java/com/chernobyte/testblock/blocks/FirstBlock.java
@@ -2,10 +2,23 @@ package com.chernobyte.testblock.blocks;
 
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.DirectionalBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.IProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
 
-public class FirstBlock extends Block {
+public class FirstBlock extends DirectionalBlock {
+
+    public static final BooleanProperty POWERED;
+
     public FirstBlock() {
         super(Properties.create(Material.IRON)
                 .sound(SoundType.METAL)
@@ -13,5 +26,38 @@ public class FirstBlock extends Block {
                 .lightValue(14)
         );
         setRegistryName("firstblock");
+        this.setDefaultState((BlockState)((BlockState)((BlockState)this.stateContainer.getBaseState()).with(FACING, Direction.SOUTH)).with(POWERED, false));
+    }
+
+//    @Override
+//    public void onBlockPlacedBy(World world, BlockPos pos, BlockState state, @Nullable LivingEntity entity, ItemStack stack)
+//    {
+//        if(entity != null)
+//        {
+//            world.setBlockState(pos, state.with(BlockStateProperties.FACING, getFacingFromEntity(pos, entity)), 2);
+//        }
+//    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext context)
+    {
+        return (BlockState)this.getDefaultState().with(FACING, context.getNearestLookingDirection().getOpposite());
+    }
+
+    public static Direction getFacingFromEntity(BlockPos clickedBlock, LivingEntity entity)
+    {
+        return Direction.getFacingFromVector((float) (entity.posX - clickedBlock.getX()), (float) (entity.posY - clickedBlock.getY()), (float) (entity.posZ - clickedBlock.getZ()));
+    }
+
+    @Override
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder)
+    {
+        super.fillStateContainer(builder);
+        builder.add(new IProperty[]{FACING, POWERED});
+    }
+
+    static
+    {
+        POWERED = BlockStateProperties.POWERED;
     }
 }
